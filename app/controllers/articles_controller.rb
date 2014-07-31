1c839de46439d1f87bdfd8424a6bde77e560ca8f
Kyle Musler <kmusler@gmail.com>
2014-07-31 20:24:06 +0000

    adding navigation links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,23 @@ def new
 end
 
 def create
-render text: params[:article].inspect
+@article = Article.new(article_params)
+
+@article.save
+redirect_to @article
 end
+
+def show
+@article = Article.find(params[:id])
+end
+
+def index
+@articles = Article.all
+end
+
+private
+def article_params
+params.require(:article).permit(:title, :text)
+end
+
 end
